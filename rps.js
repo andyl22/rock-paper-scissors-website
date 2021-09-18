447a9ee0c9ecd6849c5128dc5e352a5b55e77f4e
Andy Lau <landywl4@gmail.com>
2021-09-17 18:32:48 -0700

    Overhauled to be button-driven

--- rps.js
+++ rps.js
@@ -1,51 +1,11 @@
-async function BestOfFiveRPSGame(){
-    clearResults();
-    let humanRoundWins = 0;
-    let computerRoundWins = 0;
-    let roundCounter = 1;
-    let scoreString;
-
-    while (humanRoundWins != 3 && computerRoundWins != 3){
-        await sleep(100);
-        let roundResult;
-        roundResult = RoundofRPS(roundCounter);
-        if (roundResult==0){
-        } else if (roundResult==1) {
-            humanRoundWins++;
-            roundCounter++;
-            scoreString = generateLogString("Human", ": ", humanRoundWins);
-            log("human-score", scoreString, 2);
-        } else {
-            computerRoundWins++;
-            roundCounter++;
-            scoreString = generateLogString("Computer", ": ", computerRoundWins);
-            log("computer-score", scoreString, 2);
-        }
-    }
-
-    declareFinalWinner(humanRoundWins);
-}
-
-function declareFinalWinner(humanRoundWins) {
-    if (humanRoundWins==3) {
-        log("result", "Human Wins.");
-    } else {
-        log("result", "Computer Wins.");
-    }
-}
-  
-
-function RoundofRPS(roundCounter) {
-    let humanChoice = humanInput();
+function playRound(event){
+    let humanInput = event.currentTarget.myParam;
     let computerChoice = computerPlay();
-    return evaluateRPSResult(humanChoice, computerChoice, roundCounter);
+    return evaluateRPSResult(humanInput, computerChoice);
 }
 
 function computerPlay(){
-    return normalizeComputerPlay(Math.ceil((Math.random()*100)/33));
-}
-
-function normalizeComputerPlay(randomGenNum){
+    let randomGenNum = Math.ceil((Math.random()*100)/33);
     if (randomGenNum==1) {
         return "rock";
     } else if (randomGenNum==2) {
@@ -55,38 +15,48 @@ function normalizeComputerPlay(randomGenNum){
     }
 }
 
-function humanInput() {
-    let humanChoice = prompt("Please enter a choice of Rock, Paper, or Scissors");
-    return humanChoice.toLowerCase();
-}
-
-function evaluateRPSResult(humanChoice, computerChoice, roundCounter) {
+function evaluateRPSResult(humanChoice, computerChoice) {
     let logString;
     if (humanChoice == computerChoice) {
-        logString = generateLogString(+ roundCounter, ". ", "Tie. Replay Round.");
-        log("result"+roundCounter,logString);
-        return 0;
+        logString = generateLogString("Tie. Replay Round.");
+        log("result-round", logString);
     } else if (humanChoice == "rock" && computerChoice == "scissors") {
-        logString = generateLogString(roundCounter, ". ", `<strong>Human Wins</strong> --- Human picked ${humanChoice}. Computer picked ${computerChoice}.`);
-        log("result"+roundCounter,logString);
-        return 1;
+        logString = generateLogString(`<strong>Human Wins</strong> --- Human picked ${humanChoice}. Computer picked ${computerChoice}.`);
+        log("result-round", logString);
+        humanRoundWins++;
+        log("human-score", "Human: " + humanRoundWins);
     } else if (humanChoice == "paper" && computerChoice == "rock") {
-        logString = generateLogString(roundCounter, ". ", `<strong>Human Wins</strong> --- Human picked ${humanChoice}. Computer picked ${computerChoice}.`);
-        log("result"+roundCounter,logString);
-        return 1;
+        logString = generateLogString(`<strong>Human Wins</strong> --- Human picked ${humanChoice}. Computer picked ${computerChoice}.`);
+        log("result-round", logString);
+        humanRoundWins++;
+        log("human-score", "Human: " + humanRoundWins);
     } else if (humanChoice == "scissors" && computerChoice == "paper") {
-        logString = generateLogString(roundCounter, ". ", `<strong>Human Wins</strong> --- Human picked ${humanChoice}. Computer picked ${computerChoice}.`);
-        log("result"+roundCounter,logString);
-        return 1;
+        logString = generateLogString(`<strong>Human Wins</strong> --- Human picked ${humanChoice}. Computer picked ${computerChoice}.`);
+        log("result-round", logString);
+        humanRoundWins++;
+        log("human-score", "Human: " + humanRoundWins);
     } else {
-        logString = generateLogString(roundCounter, ". ", `<strong>Computer Wins</strong> --- Human picked ${humanChoice}. Computer picked ${computerChoice}.`);
-        log("result"+roundCounter,logString);
-        return 2;
+        logString = generateLogString(`<strong>Computer Wins</strong> --- Human picked ${humanChoice}. Computer picked ${computerChoice}.`);
+        log("result-round", logString);
+        computerRoundWins++;
+        log("computer-score", "Computer: " + computerRoundWins);
+    }
+
+    if (humanRoundWins == 5 || computerRoundWins == 5) {
+        rock.removeEventListener("click", playRound, false);
+        paper.removeEventListener("click", playRound, false);
+        scissors.removeEventListener("click", playRound, false);
+        document.getElementById("restart-button").style.visibility = 'visible';
+        if (humanRoundWins == 5) {
+            log("result", "Human Wins!");
+        } else {
+            log("result", "Computer Wins!");
+        }
     }
 }
 
-function generateLogString(prefix="", separator="", string) {
-    return prefix + separator + string + "<br>";
+function generateLogString(string) {
+    return string;
 }
 
 function log(idElement, string) {
@@ -94,20 +64,35 @@ function log(idElement, string) {
     element.innerHTML = string;
 }
 
-function clearResults() {
-    document.getElementById("result1").innerHTML = "";
-    document.getElementById("result2").innerHTML = "";
-    document.getElementById("result3").innerHTML = "";
-    document.getElementById("result4").innerHTML = "";
-    document.getElementById("result5").innerHTML = "";
+function clearData() {
+    document.getElementById("restart-button").style.visibility = 'hidden';
+    setUpButtonListeners()
+    humanRoundWins = 0;
+    computerRoundWins = 0;
+    document.getElementById("result-round").innerHTML = "";
     document.getElementById("result").innerHTML = "";
     document.getElementById("human-score").innerHTML = "Human: 0";
     document.getElementById("computer-score").innerHTML = "Computer: 0";
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+function setUpButtonListeners() {
+    let rock = document.getElementById("rock");
+    rock.addEventListener("click", playRound, false);
+    rock.myParam = "rock";
 
-let btn = document.getElementById("start-button");
-btn.addEventListener("click", BestOfFiveRPSGame);
+    let paper = document.getElementById("paper");
+    paper.addEventListener("click", playRound, false);
+    paper.myParam = "paper";
+
+    let scissors = document.getElementById("scissors");
+    scissors.addEventListener("click", playRound, false);
+    scissors.myParam = "scissors";
+}
+
+let humanRoundWins = 0;
+let computerRoundWins = 0;
+
+setUpButtonListeners();
+
+let reset = document.getElementById("restart-button");
+reset.addEventListener("click", clearData);
